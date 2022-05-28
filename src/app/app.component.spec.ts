66258c9e0bb6a15e8bb4929e722c1fb39365a6c4
nimesh vaghasiya <nimesh.vaghasiya95@live.com>
2022-05-28 14:17:24 +0530

    fix npm run test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('workspace app is running!');
+    expect(compiled.querySelector('.container h1')?.textContent).toContain('Dashboard');
   });
 });
